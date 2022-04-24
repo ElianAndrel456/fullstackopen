3e362c0853a368a0a50157986022d10d241440b9
Elian <elian7joyo7villanueva@gmail.com>
2022-04-24 09:23:30 -0500

    EstadoComplejo-

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -32,12 +32,28 @@ function Button({ handleClick, text }) {
   return <button onClick={handleClick}>{text}</button>
 }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>
+  }
+
+  return <div>button press history: {props.allClicks.join(' ')}</div>
+}
+
 const App = () => {
   const [counter, setCounter] = useState(0)
+  const [clicks, setClicks] = useState({ left: 0, right: 0, allClicks: [] })
+  const { left, right, allClicks } = clicks
 
   const increaseByOne = () => setCounter(counter + 1)
   const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
+
+  const handleLeftClick = () =>
+    setClicks({ ...clicks, left: left + 1, allClicks: allClicks.concat('L') })
+
+  const handleRightClick = () =>
+    setClicks({ ...clicks, right: right + 1, allClicks: allClicks.concat('R') })
 
   const name = 'Peter'
   const age = 10
@@ -50,6 +66,13 @@ const App = () => {
       <Button handleClick={increaseByOne} text="plus" />
       <Button handleClick={setToZero} text="zero" />
       <Button handleClick={decreaseByOne} text="minus" />
+      <div>
+        {left}
+        <Button onClick={handleLeftClick} text="left" />
+        <Button onClick={handleRightClick} text="right" />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
       <Footer />
       <hr />
       <CourseInfo />
