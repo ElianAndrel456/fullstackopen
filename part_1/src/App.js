12fa5a547e0582b48d70b582f438303062a6a5ba
Elian <elian7joyo7villanueva@gmail.com>
2022-04-23 23:44:45 -0500

    Estado complejo

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -1,9 +1,17 @@
-const Hello = (props) => {
+import { useState } from 'react'
+import { CourseInfo } from './Courseinfo/CourseInfo'
+
+const Hello = ({ age, name }) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - age
+  }
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -16,7 +24,21 @@ function Footer() {
   )
 }
 
+function Display({ counter }) {
+  return <div>Contador : {counter}</div>
+}
+
+function Button({ handleClick, text }) {
+  return <button onClick={handleClick}>{text}</button>
+}
+
 const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
   const name = 'Peter'
   const age = 10
   return (
@@ -24,8 +46,13 @@ const App = () => {
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
       <Footer />
-      
+      <hr />
+      <CourseInfo />
     </>
   )
 }
